a1218ababe319a10b442c4c2893864f40e8f118a
Charan K R <krcpsbb@gmail.com>
2022-06-12 14:37:00 +0530

    webapp formated

--- webapp.py
+++ webapp.py
@@ -14,7 +14,7 @@ loaded_model = cloudpickle.load(open('findsim.sav', 'rb'))
 
 # creating a function for Prediction
 
-def finalpred(input_data):
+def finalpred(input_data,seq):
 
 
     
@@ -26,7 +26,7 @@ def finalpred(input_data):
     titles=titles.values.tolist()
     i=titles.index(input_data)
 
-    prediction = loaded_model(df,i,del_sequels = True, verbose = True)
+    prediction = loaded_model(df,i,del_sequels = seq, verbose = False)
     #print(prediction)
 
     return prediction
@@ -35,9 +35,24 @@ def finalpred(input_data):
   
 def main():
     
-    st.set_page_config(page_title='WSWN',page_icon=img)
+    st.set_page_config(page_title='What should I watch Next',page_icon=img)
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    with st.sidebar:
+        image = Image.open('wswn.jpg')
+        st.image(image)
+        st.subheader('Type in the name of the movie you last watched and get 5 suggestions you might like!!')
+        st.write('A popularity+content based recommender which suggests movies similar to the user’s favourite one using keywords of movies’ genre, era, plot, cast and more.')
+        st.write("Notebook for creating Model: [link](https://share.streamlit.io/mesmith027/streamlit_webapps/main/MC_pi/streamlit_app.py)")
     # giving a title
-    st.title('What should I watch Next')
+    st.title('What should I watch Next?')
+    st.markdown("<h5 style='text-align: right; color: red;'>Created by Charan K R</h1>", unsafe_allow_html=True)
+    #st.subheader('-created by Charan K R')
     
     
     # getting the input data from the user
@@ -52,13 +67,19 @@ def main():
     
     
     # creating a button for Prediction
-    
+    seq=False
+
+    if st.checkbox('Dont Recommend Sequels'):
+        seq=True
+
     if st.button('You might like...'):
-        pred = finalpred(Movie)
+        pred = finalpred(Movie,seq)
         moviepred=[]
         for i in range(len(pred)):
             moviepred.append(pred[i][0])
-        st.write(moviepred)
+        for j in range(5):
+            st.write(moviepred[j])
+
     
     #st.success(pred[0][0])
     
